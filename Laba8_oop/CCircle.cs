1479f7023ca0fb01bbdd2ebe9a62a6cd5388e29d
Edik Daukaev <daukaev.eduard@mail.ru>
2020-12-28 06:42:11 +0500

    Изменение 1

---
+++ Laba8_oop/CCircle.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Laba8_oop
+{
+    class CCircle
+    {
+    }
+}
